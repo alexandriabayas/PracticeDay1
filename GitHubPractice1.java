6b6eb1c5cb19b1101cfcd4542455b7e07583106d
alexandriabayas <45398974+alexandriabayas@users.noreply.github.com>
2018-11-27 12:20:14 -0500

    Created ArrayLists
    Created 2 ArrayLists and added values

--- GitHubPractice1.java
+++ GitHubPractice1.java
@@ -1,6 +1,14 @@
 import java.util.ArrayList;
 public class GitHubPractice1{
 	public static void main(String[]args){
-	
+
+		ArrayList<Integer> list1 = new ArrayList<>();
+		for(int i = 0; i <= 10; i+=2)
+			list1.add(i);
+		ArrayList<Integer> list2 = new ArrayList<>();
+		for(int i = 1; i <= 10; i+=2)
+			list2.add(i);
+
+
 	}
 }
