5492f8c014fa56e80f4d5c823cf8f770821c25e2
alexandriabayas <45398974+alexandriabayas@users.noreply.github.com>
2018-11-27 12:25:27 -0500

    Created method
    Created method to combine two ArrayLists, printed lists and new list from method

--- GitHubPractice1.java
+++ GitHubPractice1.java
@@ -3,12 +3,30 @@ public class GitHubPractice1{
 	public static void main(String[]args){
 
 		ArrayList<Integer> list1 = new ArrayList<>();
-		for(int i = 0; i <= 10; i+=2)
-			list1.add(i);
 		ArrayList<Integer> list2 = new ArrayList<>();
-		for(int i = 1; i <= 10; i+=2)
-			list2.add(i);
+
+		for(int i = 0; i < 10; i++){
+			if(i%2 == 0)
+				list1.add(i);
+			else
+				list2.add(i);
+		}
+
+		System.out.println(list1);
+		System.out.println(list2);
+
+		System.out.println(combine(list1, list2));
 
 
 	}
+
+	public static ArrayList<Integer> combine(ArrayList<Integer> list1, ArrayList<Integer> list2){
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 0; i < list1.size(); i++){
+			list.add(list1.get(i));
+			list.add(list2.get(i));
+		}
+		return list;
+	}
+
 }
